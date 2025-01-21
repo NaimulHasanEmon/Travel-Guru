f91b4c12f79039e508b333c9122c428984c5e9d9
Md. Naimul Hasan Emon <hasan15-5601@diu.edu.bd>
2025-01-22 04:05:11 +0600

    Background contrast updated

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,7 +3,7 @@ import bg from '../../assets/images/photos/Rectangle 1.png';
 const Banner = () => {
     return (
         <div
-        className='w-full h-screen bg-center bg-no-repeat bg-cover opacity-45'
+        className='w-full h-screen bg-center bg-no-repeat bg-cover opacity-25 filter contrast-150'
         style={{backgroundImage: `url(${bg})`}}>
         </div>
     );
