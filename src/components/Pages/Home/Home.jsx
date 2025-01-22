925966bada7f1420725e092687ba4fa977aaa703
Md. Naimul Hasan Emon <hasan15-5601@diu.edu.bd>
2025-01-22 17:14:41 +0600

    Card and bottom arrow buttons added

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,43 +1,66 @@
 import BannerText from "../Shared/BannerText/BannerText";
 import PlaceCard from "../Shared/PlaceCard/PlaceCard";
-import Banner from '../Home/Banner/Banner';
+import Banner from "../Home/Banner/Banner";
 import { useEffect, useState } from "react";
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
 
-import coxBazar from '../../../assets/images/photos/CoxBazar.jpg';
-import sreemongol from '../../../assets/images/photos/Sreemongol.png';
-import sundarban from '../../../assets/images/photos/sundorbon.png';
-import sajek from '../../../assets/images/photos/Sajek.png';
+import coxBazar from "../../../assets/images/photos/CoxBazar.jpg";
+import sreemongol from "../../../assets/images/photos/Sreemongol.png";
+import sundarban from "../../../assets/images/photos/sundorbon.png";
+import sajek from "../../../assets/images/photos/Sajek.png";
 
 const Home = () => {
-  const [photo, setPhoto] = useState(coxBazar);
-  const photos = [coxBazar, sreemongol, sundarban, sajek];
   const [photoIndex, setPhotoIndex] = useState(0);
+  const photos = [coxBazar, sreemongol, sundarban, sajek];
 
   useEffect(() => {
     const interval = setInterval(() => {
       setPhotoIndex((prevIndex) => (prevIndex + 1) % photos.length);
     }, 5000);
-  
+
     return () => clearInterval(interval);
   }, [photos]);
+
+  const handleLeftArrow = () => {};
+
+  const handleRightArrow = () => {};
+
+  // The selected photo based on the current index
+  const currentPhoto = photos[photoIndex];
 
   return (
     <div className='flex'>
       {/* Background Banner */}
-      <Banner
-      photo={photos[photoIndex]}
-      ></Banner>
+      <Banner photo={currentPhoto} />
 
       {/* Left Column */}
       <div>
-        <BannerText></BannerText>
+        <BannerText />
       </div>
 
       {/* Right Column */}
       <div>
-        <PlaceCard
-        photo={photos[photoIndex]}
-        ></PlaceCard>
+        <div>
+          <PlaceCard
+            photo={currentPhoto} // Send the current photo to PlaceCard
+            photos={photos} // Send the entire photos array to PlaceCard
+          />
+        </div>
+
+        {/* Navigation Arrows */}
+        <div className='absolute bottom-[50px] right-[550px] flex gap-2'>
+          <div
+          onClick={() => handleLeftArrow()}
+          className='text-4xl bg-slate-600 rounded-full text-white hover:bg-teal-400 cursor-pointer'>
+            <IoIosArrowBack />
+          </div>
+          <div
+          onClick={() => handleRightArrow()}
+          className='text-4xl bg-slate-600 rounded-full text-white hover:bg-teal-400 cursor-pointer'>
+            <IoIosArrowForward />
+          </div>
+        </div>
       </div>
     </div>
   );
