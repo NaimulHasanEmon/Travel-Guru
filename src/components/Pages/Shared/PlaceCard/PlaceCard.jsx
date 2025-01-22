1e2d9ce7d71b87e31476917cd132927c7ed9ce8e
Md. Naimul Hasan Emon <hasan15-5601@diu.edu.bd>
2025-01-22 18:54:12 +0600

    Photo animation updated

--- src/components/Pages/Shared/PlaceCard/PlaceCard.jsx
+++ src/components/Pages/Shared/PlaceCard/PlaceCard.jsx
@@ -1,6 +1,6 @@
 const PlaceCard = ({ photo, photos }) => {
   return (
-    <div className='absolute top-1/4 right-[0px] px-20 w-1/2 flex gap-64'>
+    <div className='absolute top-1/4 right-[0px] px-20 w-1/2 flex'>
       <div className='rounded-xl'>
         <img
           src={photo}
@@ -8,17 +8,17 @@ const PlaceCard = ({ photo, photos }) => {
           className='h-96 w-68 rounded-xl object-cover'
         />
       </div>
-      <div className='flex flex-col gap-3 justify-center'>
+      <div className='flex flex-col gap-3 justify-center ml-52'>
         {photos.map((pic, index) => {
           const isActive = pic === photo;
           return (
             <img
               key={index}
               src={pic}
-              className={`h-20 mx rounded-lg w-full object-cover ${
+              className={`h-20 mx rounded-lg object-cover transition-all duration-700 ease-in-out transform ${
                 isActive
-                  ? "border-[3px] border-teal-400"
-                  : "hover:border-2"
+                  ? "border-[3px] border-teal-400 h-36 w-24 translate-x-[-120px] absolute"
+                  : "hover:border-2 w-20 hover:h-24 hover:translate-x-[15px]"
               }`}
               alt=''
             />
